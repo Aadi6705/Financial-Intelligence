3e7df05446b0b64d6e2f64b98f53526ab7281b0a
white_w6lf <asaditya6705.gmail.com>
2026-07-15 19:05:32 +0530

    Initial Flask application setup

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-import python as py
-j
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
